03c5ed09f192f3b73d380dcae872a25d4d51cd36
Andrea Martella <52160332+Andream98@users.noreply.github.com>
2024-05-10 09:46:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,12 +7,22 @@ import FormElement from "./formElement.vue";
 import FormGroup from "./formGroup.vue";
 
 const install = (app, options = {}) => {
-	if (options.fields) {
-		options.fields.forEach((field) => {
-			if (typeof field.name !== "undefined") {
-				Vue.component(field.name, field);
+	if (options) {
+		if (options.fields) {
+			options.fields.forEach((field) => {
+				if (typeof field.name !== "undefined") {
+					Vue.component(field.name, field);
+				}
+			});
+		}
+		
+		if (options.validators) {
+			for (let key in options.validators) {
+				if ({}.hasOwnProperty.call(options.validators, key)) {
+					validators[key] = options.validators[key];
+				}
 			}
-		});
+		}
 	}
 	app.component("VueFormGenerator", component);
 };
